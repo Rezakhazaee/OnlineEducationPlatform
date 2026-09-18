457b74da4b442f976b2883845709cf2a85a45757
rezapardis2023 <reza.pardis.2023@gmail.com>
2026-09-18 14:15:08 +0000

    Add lesson progress migration

---
+++ SampleApp/BackEnd/Migrations/20260918141235_AddEnrollmentLessonProgress.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnrollmentLessonProgress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnrollmentLessonProgresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EnrollmentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CourseLessonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrollmentLessonProgresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnrollmentLessonProgresses_CourseLessons_CourseLessonId",
+                        column: x => x.CourseLessonId,
+                        principalTable: "CourseLessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnrollmentLessonProgresses_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrollmentLessonProgresses_CourseLessonId",
+                table: "EnrollmentLessonProgresses",
+                column: "CourseLessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrollmentLessonProgresses_EnrollmentId",
+                table: "EnrollmentLessonProgresses",
+                column: "EnrollmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnrollmentLessonProgresses");
+        }
+    }
+}
